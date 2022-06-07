bad639026cc665bbd7ba67dfc03f46f2bec3cae6
Damilola Olaiya <daolaiya@gmail.com>
2022-06-07 19:55:52 -0400

    hjsrt gfvuljke

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
-const BASE_URL = process.env.NODE_ENV === "production" ? "https://midas-movies-olaiya.herokuapp.com/" : process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.NODE_ENV === "production" ? "https://midas-movies-olaiya.herokuapp.com" : process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 class MidasApi {
     static token;
