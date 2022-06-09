3d997e6f010c249e94d6d58234ede8474f5f8a3b
Damilola Olaiya <daolaiya@gmail.com>
2022-06-09 12:56:39 -0400

    Changing logout

--- client/src/components/LogOut.js
+++ client/src/components/LogOut.js
@@ -1,15 +1,19 @@
 import React, {useContext, useEffect} from "react";
-import {Redirect } from "react-router-dom";
+import {Redirect, useHistory} from "react-router-dom";
 import CountContext from "../countContext";
 
 function LogOut(){
-    const {token, setToken} = useContext(CountContext);
+    const {setToken} = useContext(CountContext);
+    const history = useHistory();
 
     useEffect(() => {
         setToken(null);
-    }, [token, setToken]);
+        history.push("/");
+    }, [setToken]);
 
-    return <Redirect to="/"/>;
+
+    // return <Redirect to="/"/>;
+    return <div></div>
 }
 
 export default LogOut;
